a97e299c32e734534cd1fb18b920b633d65ac4d6
maycon <maycon.aguiar@gmail.com>
2018-12-04 17:03:28 -0200

    ajustes

--- src/app/login/login-form/login-form.component.ts
+++ src/app/login/login-form/login-form.component.ts
@@ -1,33 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { Validators } from '@angular/forms';
-import { BaseFormComponent } from '../../comum/base-form/base-form.component';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss']
 })
-export class LoginFormComponent extends BaseFormComponent implements OnInit {
+export class LoginFormComponent implements OnInit {
 
   constructor(
     private afAuth: AngularFireAuth,
-    private router: Router,
-    private formBuilder: FormBuilder) {
-      super();
+    private router: Router) {
     }
 
   email = '';
   senha = '';
-  // formulario: FormGroup;
 
   ngOnInit() {
-
-    this.formulario = this.formBuilder.group({
-      email: [ null, [Validators.required, Validators.email]],
-      senha: [null, [Validators.required]]});
   }
 
   novoUsuario() {
@@ -35,15 +25,15 @@ export class LoginFormComponent extends BaseFormComponent implements OnInit {
     this.router.navigate(['/login/cadastro']);
   }
 
-  submit() {
-    this.afAuth.auth.signInWithEmailAndPassword(this.formulario.value.email, this.formulario.value.senha).then(ok => {
+  submit(f) {
+    this.afAuth.auth.signInWithEmailAndPassword(this.email, this.senha).then(ok => {
       this.router.navigate(['/dashboard']);
     })
     .catch(c => {
       alert('Ihhh deu probleminha aqui! \n Erro: ' + c.message);
       });
 
-    this.email = '';
-    this.senha = '';
+    // this.email = '';
+    // this.senha = '';
   }
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -8,8 +8,6 @@ import { FirebaseConfig } from '../../environments/firebase.config';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { LoginFormComponent } from './login-form/login-form.component';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ComumModule } from '../comum/comum.module';
 
